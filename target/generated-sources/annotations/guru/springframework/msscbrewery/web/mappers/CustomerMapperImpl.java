6b94ea9e8e31e7c11c99c76903082a6692c30d77
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-13 15:28:53 +0200

    Enriched the BeerDto and the Beer entity with date fields, using different date types. Implemented DateMapper class. Instructed the BeerMapper interface to utilize the DateMapper class in the @Mapper annotation.

--- target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-13T15:12:34+0200",
+    date = "2021-02-13T15:24:39+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
